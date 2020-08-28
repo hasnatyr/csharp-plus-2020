0814b5b80856f023aab93e55ef370545b53f0c8b
DESKTOP-KDVTBFD\Auf_Ausha <abul.hasnat@studerande.plushogskolan.se>
2020-08-28 09:52:20 +0200

    Session 02

---
+++ Session02/Session02/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Session01_Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            }
+
+            #region console interaction
+
+
+
+            Console.ReadLine();
+
+            Print("value to print");
+
+            #endregion
+
+            #if Debug
+                
+            #endif
+        }
+    }
+}
